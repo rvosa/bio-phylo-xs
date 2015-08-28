995002f43303a06f95d18d8569c257b1be8da674
rvosa <rutgeraldo@gmail.com>
2015-08-28 17:58:52 +0200

    made node objects

--- types.h
+++ types.h
@@ -1,14 +1,18 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-typedef struct {
-     char* name;
-     int legs;
-} Animal;
+int idpool = 1;
 
 typedef struct {
-	Animal animal;
-	char* owner;
-} Cat;
+     int id;
+} Identifiable;
+
+typedef struct Node {
+	Identifiable identifiable;
+	double branch_length;
+	SV* parent;
+	SV* first_daughter;
+	SV* next_sister;
+} Node;
 
 #endif
