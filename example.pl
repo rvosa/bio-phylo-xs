df390d4fac77b2107a62b8dbc8fb7e5fa6190a6d
rvosa <rutgeraldo@gmail.com>
2015-08-28 17:59:05 +0200

    apart from free-ing, this works

--- example.pl
+++ example.pl
@@ -1,9 +1,19 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Cat;
+use Node;
+use Devel::Peek;
 
 # amazingly, this all works
-my $cat = Cat->new( '-name' => 'Rutger', '-legs' => 2 );
-$cat->set_owner('Alva');
-print $cat->get_owner, "\n";
+my $child = Node->new( '-branch_length' => 0.2342 );
+print $child->get_id, "\n";
+print $child->get_branch_length, "\n";
+
+my $parent = Node->new( '-branch_length' => 0.7346 );
+print $parent->get_id, "\n";
+print $parent->get_branch_length, "\n";
+
+$child->set_parent($parent);
+
+print $child->get_parent->get_branch_length, "\n";
+
