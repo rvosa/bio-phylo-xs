e83aed679480e57d0fa7197a272af8b02cb196d0
Rutger Vos <rutgeraldo@gmail.com>
2015-08-30 00:36:59 +0200

    added object type test

--- example.pl
+++ example.pl
@@ -2,10 +2,21 @@
 use strict;
 use warnings;
 use Node;
-use Bio::Phylo::Forest::Node;
+use Data::Dumper;
 use Benchmark qw(:all);
+use Bio::Phylo::Util::CONSTANT qw(:objecttypes);
+use Devel::Peek;
 
 # C nodes
 my $child = Node->new;
-$child->set_raw_parent(Node->new);
-$child->get_parent->set_raw_child($child);
+my $parent = Node->new;
+$child->set_raw_parent($parent);
+$parent->set_raw_child($child);
+$parent->set_raw_child( Node->new => 1 );
+print Dumper($parent->get_children);
+
+Dump($child);
+
+if ( $child->_type == _NODE_ ) {
+	print "It's a node\n";
+}
