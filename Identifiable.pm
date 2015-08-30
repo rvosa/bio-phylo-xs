a2f85d667a644cc30a0db2d78d3001150982d727
Rutger Vos <rutgeraldo@gmail.com>
2015-08-31 00:02:37 +0200

    now calls all SUPER::initialize methods to populate object

--- Identifiable.pm
+++ Identifiable.pm
@@ -1,11 +1,13 @@
 package Identifiable;
 use Inline C => './identifiable.c';
+use Bio::Phylo::Util::MOP;
 
 sub new {
-	my $class = shift;
-	my %args = @_;
+	my $class = shift;	
 	my $self = $class->create;
-	$self->identify;
+	my $init = Bio::Phylo::Util::MOP->get_implementations( 'initialize', $class );
+	$_->{code}->($self) for @$init;
+	my %args = @_;
 	for my $key ( keys %args ) {
 		my $method = $key;
 		$method =~ s/^-/set_/;
