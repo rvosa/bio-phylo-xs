03ae5a99cbc8e3c75e37842ac352566a1ae9e877
rvosa <rutgeraldo@gmail.com>
2015-08-31 15:22:48 +0200

    refactored to benchmark

--- example.pl
+++ example.pl
@@ -8,27 +8,38 @@ use Benchmark qw(:all);
 use Bio::Phylo::Util::CONSTANT qw(:objecttypes);
 use Devel::Peek;
 
-my $tree = Tree->new;
-my $root = Node->new( 
-	'-rank'          => 'genus', 
-	'-branch_length' => 0.342, 
-	'-tree'          => $tree,
-	'-name'          => 'root',
-);
-$tree->insert($root);
+sub run {
+	my $prefix = shift || '';
+	my $tree_package = $prefix . 'Tree';
+	my $node_package = $prefix . 'Node';
 
-for ( 1 .. 2 ) {
-	my $c = Node->new( 
-		'-rank'          => 'species', 
-		'-branch_length' => 0.789, 
+	my $tree = $tree_package->new;
+	my $root = $node_package->new( 
+		'-rank'          => 'genus', 
+		'-branch_length' => 0.342, 
 		'-tree'          => $tree,
-		'-name'          => "node$_",
+		'-name'          => 'root',
 	);
-	$root->set_raw_child( $c );
-	$c->set_raw_parent( $root );
-	$tree->insert($c);
+	$tree->insert($root);
+
+	for ( 1 .. 2 ) {
+		my $c = $node_package->new( 
+			'-rank'          => 'species', 
+			'-branch_length' => 0.789, 
+			'-tree'          => $tree,
+			'-name'          => "node$_",
+		);
+		$root->set_raw_child( $c );
+		$c->set_raw_parent( $root );
+		$tree->insert($c);
+	}
+
+	$_->get_rank, for @{ $root->get_children };
+	$tree->get_root->get_rank;
+	$tree->to_newick;
 }
 
-print $_->get_rank, "\n" for @{ $root->get_children };
-print $tree->get_root->get_rank, "\n";
-print $tree->to_newick;
+timethese(1000, {
+   'C'    => sub { run() },
+   'Perl' => sub { run('Bio::Phylo::Forest::') },
+});
