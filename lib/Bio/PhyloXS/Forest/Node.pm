9e99a371d45af17583fb22e6a320716b3933e205
Rutger Vos <rutgeraldo@gmail.com>
2015-09-06 21:52:29 +0200

    added AUTOLOAD to dispatch to pure perl decorators

--- lib/Bio/PhyloXS/Forest/Node.pm
+++ lib/Bio/PhyloXS/Forest/Node.pm
@@ -1,4 +1,13 @@
 package Bio::PhyloXS::Forest::Node;
 use base qw'Bio::PhyloXS::Listable Bio::Phylo::Forest::Node';
 use Inline C => 'src/Node.c';
+
+our $AUTOLOAD;
+sub AUTOLOAD {
+	my $self = shift;
+	my $method = $AUTOLOAD;
+	$method =~ s/.+:/Bio::Phylo::Forest::Node::/;
+	$self->$method(@_);
+}
+
 __PACKAGE__;
