918f7068239a0294e14d526b89cc7ccb7409997a
Rutger Vos <rutgeraldo@gmail.com>
2015-09-13 16:23:33 +0900

    adding magical numbers for supported data types

--- src/Datatype.h
+++ src/Datatype.h
@@ -3,6 +3,12 @@
 
 # include "src/Writable.h"
 
+#define _DNA_ 1
+#define _RNA_ 2
+#define _PROTEIN_ 3
+#define _STANDARD_ 4
+#define _CONTINUOUS_ 5
+
 typedef struct {
 	Writable writable;
 	char missing;
