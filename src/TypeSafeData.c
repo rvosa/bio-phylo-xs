dae0cc9629ca910877213d0f36f16d800b7bc434
Rutger Vos <rutgeraldo@gmail.com>
2017-10-13 12:13:44 +0200

    need to re-implement constructor and get/set type due to delegation instead of inheritance

--- src/TypeSafeData.c
+++ src/TypeSafeData.c
@@ -15,11 +15,34 @@ void initialize_typesafedata(TypeSafeData* self){
 
 	// create datatype object
 	Newx(self->datatype,1,Datatype);
-	initialize_datatype(self->datatype);
+	initialize_writable((Writable*)self->datatype);
+	self->datatype->missing = '?';
+	self->datatype->gap = '-';
+	self->datatype->datatype = 0;
+	((Identifiable*)self->datatype)->_type = _DATATYPE_;
+	((Identifiable*)self->datatype)->_container = _NONE_;
+	((Identifiable*)self->datatype)->_index = _DATATYPE_IDX_;		
 }
 
 TypeSafeData* set_type(TypeSafeData* self, char* type) {
-	_to_set_type(self->datatype,type);
+	if ( tolower(type[0]) == 'd' ) {
+		self->datatype->datatype = _DNA_;
+	}
+	else if ( tolower(type[0]) == 'r' ) {
+		self->datatype->datatype = _RNA_;
+	}
+	else if ( tolower(type[0]) == 'p' ) {
+		self->datatype->datatype = _PROTEIN_;
+	}
+	else if ( tolower(type[0]) == 's' ) {
+		self->datatype->datatype = _STANDARD_;
+	}
+	else if ( tolower(type[0]) == 'c' ) {
+		self->datatype->datatype = _CONTINUOUS_;
+	}
+	else {
+		warn("Unsupported data type");
+	}
 	return self;
 }
 
@@ -42,7 +65,28 @@ TypeSafeData* set_gap(TypeSafeData* self, char gap) {
 }
 
 char* get_type(TypeSafeData* self) {
-	return _to_get_type(self->datatype);
+	char* retval;
+	switch(self->datatype->datatype) {
+		case _DNA_:
+			retval = "dna";
+			break;
+		case _RNA_:
+			retval = "rna";
+			break;
+		case _PROTEIN_:
+			retval = "protein";
+			break;
+		case _STANDARD_:
+			retval = "protein";
+			break;
+		case _CONTINUOUS_:
+			retval = "continuous";
+			break;
+		default:
+			warn("Datatype undefined");
+			break;
+	}
+	return retval;
 }
 
 Datatype* get_type_object(TypeSafeData* self) {
